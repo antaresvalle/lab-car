4a4dfd9aabef5306a38fcfd1c6bc0ece9726f35b
antaresvalle <antaresvll561@gmail.com>
2018-06-16 20:55:46 -0500

    Mobile v1.0

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 // La funcionalidad de tu proyecto
+// document.addEventListener('click', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     console.log(elems);
+//     var instances = M.Sidenav.init(elems, options);
+//   });

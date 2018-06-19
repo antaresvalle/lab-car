2edd874f9fb70cca036d1b02f461ff2be8c15d92
antaresvalle <antaresvll561@gmail.com>
2018-06-19 10:05:25 -0500

    Modals

--- js/app.js
+++ js/app.js
@@ -3,4 +3,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems, {opacity: 0.5});
-  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('select');
+	var instances = M.FormSelect.init(elems, {dropdownOptions: "option1"});
+});
